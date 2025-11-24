6ef04d292a0863b270752a36e9a495358944a0f3
xaavic <xacaballe@gmail.com>
2025-11-24 19:39:17 +0100

    Modificar login, per guardar estat de sessió

--- randomecommerce/src/main/java/randomecommerce/randomecommerce/service/UserService.java
+++ randomecommerce/src/main/java/randomecommerce/randomecommerce/service/UserService.java
@@ -11,17 +11,24 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    //Registra un usuari nou si no existeix
     public boolean registrarUsuari(String username, String password) {
         if (userRepository.findByUsername(username) != null) {
-            return false; // ja existeix
+            return false;
         }
         User user = new User(username, password);
         userRepository.save(user);
         return true;
     }
 
+    //Valida si l'usuari existeix i la contrasenya es correcta
     public boolean validaUsuari(String username, String password) {
         User user = userRepository.findByUsername(username);
         return user != null && user.getPassword().equals(password);
+    }
+    
+    //Obtenir usuari per el seu nom
+    public User getUser(String username) {
+        return userRepository.findByUsername(username);
     }
 }
